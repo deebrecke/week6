b81a82ec392cd64e31b17c69309a5f0654d8b78c
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-02-07 17:19:36 -0500

    Added some templates

--- index.php
+++ index.php
@@ -15,8 +15,34 @@ require_once("vendor/autoload.php");
 $f3 = Base::instance();
 
 //anonymous function--not named function
-$f3->route('GET /', function(){
-    //echo '<h1>Week 6</h1>';
+$f3->route('GET /', function($f3){
+
+    //add key-value pairsto the f3 "hive"
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+
+    //add food and color
+    $f3->set('food', 'sushi');
+    $f3->set('color', 'purple');
+
+    $f3->set('temp', 67);
+    $f3->set('radius', 10);
+
+//define and array of fruits
+    $fruits = array("apple", "banana", "orange");
+
+    $f3->set('fruits', $fruits);
+
+//    foreach($fruits as $fruit){
+//        echo $fruit;
+//    }
+
+    $f3->set('colors', array("red", "blue" "yellow"));
+
+
+
+
     $view = new Template();
     echo $view->render('views/info.html');
 });
